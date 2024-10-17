6f84c6b62e09a15fba88b89bc7758cb5ce240b40
Al Bowles <bowlesan@proton.me>
2024-10-16 21:53:18 -0500

    feat: Stub out NewCampaign function

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,24 +1,34 @@
 package exchange
 
 import (
-
-	"os"
 	"errors"
+	"os"
 	"strings"
+
 	"github.com/go-chat-bot/bot"
 )
 
 var (
-	ALLOWED_ADMINS = strings.Split(os.Getenv("EXCHANGE_ALLOWED_ADMINS"),",")
+	ALLOWED_ADMINS = strings.Split(os.Getenv("EXCHANGE_ALLOWED_ADMINS"), ",")
 )
+
+// A Participant is a user that has chosen to participate in a gift exchange Campaign
+type Participant struct {
+	user           *bot.User
+	address, notes string
+	recipientID    int
+}
+
+type Campaign struct {
+	channel      string
+	participants []*Participant
+}
 
 // initialize starts a new gift exchange Campaign
 func initialize(command *bot.Cmd) (msg string, err error) {
-	if (isAllowedAdmin(command.User)) {
-		// check for existing campaign in this channel
-		// create new one if not
+	if isAllowedAdmin(command.User) {
+		_, err = NewCampaign(command)
 		msg = "welcome to satans"
-		err = nil
 	} else {
 		msg = "no satans for u"
 		err = errors.New("unauthorized")
@@ -26,10 +36,21 @@ func initialize(command *bot.Cmd) (msg string, err error) {
 	return
 }
 
+func NewCampaign(cmd *bot.Cmd) (*Campaign, error) {
+	// check for existing campaign in this channel
+	// create new one if not
+	// SELECT * FROM campaigns WHERE 'channel' = cmd.Channel
+	c := Campaign{
+		channel:      cmd.Channel,
+		participants: nil,
+	}
+	return &c, nil
+}
+
 // isAllowedAdmin returns true if isNickservRegistered  AND user in EXCHANGE_ALLOWED_ADMINS
 func isAllowedAdmin(user *bot.User) bool {
 	for _, value := range ALLOWED_ADMINS {
-		if (value == user.Nick) {
+		if value == user.Nick {
 			return true
 		}
 	}
